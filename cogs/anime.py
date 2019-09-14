df2ae445ba8b1b7d1560292ddcb3b6b85b9a6436
EthanSk13s <ethanmiranda11@gmail.com>
2019-09-14 15:46:44 -0700

    add manga command

--- cogs/anime.py
+++ cogs/anime.py
@@ -6,18 +6,32 @@ from discord.ext import commands
 class Anime(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+        self.path = 'https://kitsu.io/api/edge/'
 
     @commands.command()
     async def anime(self, ctx, *, anime):
         async with aiohttp.ClientSession() as s:
-            async with s.get(f"https://kitsu.io/api/edge/anime?filter[text]={anime}") as r:
-                response = await r.json(content_type=None)
+            async with s.get(f"{self.path}anime?filter[text]={anime}") as r:
+                response = await r.json()
 
         embed = discord.Embed(title=response['data'][0]['attributes']['canonicalTitle'],
         description=response['data'][0]['attributes']['synopsis'])
         embed.set_thumbnail(url=response['data'][0]['attributes']['posterImage']['large'])
 
         await ctx.send(embed=embed)
+
+    @commands.command()
+    async def manga(self, ctx, *, manga):
+        async with aiohttp.ClientSession() as s:
+            async with s.get(f"{self.path}manga?filter[text]={manga}") as r:
+                response = await r.json()
+
+        embed = discord.Embed(title=response['data'][0]['attributes']['canonicalTitle'],
+        description=response['data'][0]['attributes']['synopsis'])
+        embed.set_thumbnail(url=response['data'][0]['attributes']['posterImage']['large'])
+
+        await ctx.send(embed=embed)
+        
 
 def setup(bot):
     bot.add_cog(Anime(bot))            
