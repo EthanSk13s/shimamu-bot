f061a1a31c680dce07b44dd09f39a910b73c45ca
EthanSk13s <ethanmiranda11@gmail.com>
2019-10-19 14:16:21 -0700

    added search command

--- cogs/anime.py
+++ cogs/anime.py
@@ -31,8 +31,31 @@ class Anime(commands.Cog):
         embed.set_thumbnail(url=response['data'][0]['attributes']['posterImage']['large'])
 
         await ctx.send(embed=embed)
-        
 
+    @commands.command()
+    async def search(self, ctx, Type, *, query):
+        async with aiohttp.ClientSession() as s:
+            types = {
+                'anime': s.get(f"{self.path}anime?filter[text]={query}"),
+                'manga': s.get(f"{self.path}manga?filter[text]={query}")
+            }
+            if Type in types:
+                async with types.get(Type) as r:
+                    responses = await r.json()
+            else:
+                await ctx.send("Unknown attribute! Attribute can be text or genre!")
+        embed = discord.Embed(title=f"Results for {query}")
+
+        for x, response in enumerate(responses['data']):
+            x += 1
+            if x <= 9:
+                embed.add_field(name=responses['data'][x]['attributes']['canonicalTitle'],
+                value=u'\u200b', inline=False)
+            else:
+                break
+
+        await ctx.send(embed=embed)
+            
 def setup(bot):
     bot.add_cog(Anime(bot))            
     
