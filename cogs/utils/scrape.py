0a96a445f78e854b500e1a1ca8de216737e82c83
EthanSk13s <ethanmiranda11@gmail.com>
2019-10-27 21:28:41 -0700

    add scrape functions

--- cogs/utils/scrape.py
+++ cogs/utils/scrape.py
@@ -5,18 +5,47 @@ class Scraper():
         self.url = url
         self.soup = bs.BeautifulSoup(url, 'lxml')
 
-class CharaScraper(Scraper):
-    def __init__(self, url):
-        super().__init__(url)
-    
+class CharaScraper(Scraper):   
     def summary(self):
-
         desc = self.soup.find('table', 
         {'class':'wikitable',
         'style': 'width:100%; text-align:center; text-size-adjust: none; margin-top:0;'}
-        )
-        pog = str(desc.find_all('td'))    
-        thing = pog.strip('[').strip(']').strip('<td>').strip('</td>')
+        ) 
 
-        return thing
+        return desc.text
+
+    def title(self):
+        title = self.soup.find('div', {'class': 'char-title'})
+        tl1 = "This character's title is untranslated."
+        tl2 = "This title is an unofficial, amateur translation."
+        tl3 = "This title is a translation sourced from Granblue Fantasy"
+
+        return title.text.strip(tl1).strip(tl2).strip(tl3)
+
+    def name(self):
+        name = self.soup.find('div', {'class': 'char-name'})
+
+        return name.text
+
+    def hp(self):
+        hp = self.soup.find('td', {'style': 'width:65%;'})
+
+        return hp.text
+
+    def atk(self):
+        table = self.soup.find('table', {'class': 'wikitable',
+        'style': 'width: 100%; text-align:center; margin-top:2px; margin-left:auto; margin-right:auto;'})
+
+        atk = table.find_all('td')
+
+        return atk[1].text
+
+    def image(self):
+        imgs = self.soup.find('a', {'class': 'image'})
+        img = imgs.find_all('img')
+
+        return f"https://gbf.wiki{img[0]['src']}"
+
+        
+
 
